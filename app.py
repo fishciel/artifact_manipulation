aa4f45d0bc1a6b51d9f94217ef67c7d1ffec3151
fishciel <fishciel@users.noreply.github.com>
2025-07-25 22:57:43 -0700

    Basic pattern detection implemented (no normalization).

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 import pandas as pd
+import collections
 from datetime import datetime
+
+""" 
+Basic setup:
+    - title, headers, boxes
+    - add entries
+    - save entries
+    - display current entries
+"""
 
 # Name/title
 st.title("Artifact RNG Pattern Tracker")
@@ -35,7 +44,7 @@ if submitted:
     })
     st.success("Entry added!")
 
-# Display
+# Display entries
 st.header("Upgrade Log")
 
 if st.session_state.upgrade_log:
@@ -43,3 +52,35 @@ if st.session_state.upgrade_log:
     st.dataframe(df, use_container_width=True)
 else:
     st.write("No data logged yet.")
+
+"""
+Pattern Detection
+"""
+
+def get_upgrade_pairs(log):
+    """
+    Go through each entry and get the upgraded_line value (1-4)
+    Convert to string and concat
+    ex.
+    ["1", "4", "4", "1", "2", "3"]
+    ["14", "41", "23"], these pairs are returned
+    """
+    lines = [str(entry["upgraded_line"]) for entry in log]
+    pairs = [lines[i] + lines[i+1] for i in range(len(lines) - 1)]
+    return pairs
+
+# Check for at least 2 upgrades (a pair)
+if st.session_state.upgrade_log and len(st.session_state.upgrade_log) > 1:
+    st.header("Upgrade patterns detected")
+
+    # Display pairs and occurrences
+    pairs = get_upgrade_pairs(st.session_state.upgrade_log)
+    pair_counts = collections.Counter(pairs)
+
+    df_pairs = pd.DataFrame(pair_counts.items(), columns=["Pattern", "Count"])
+    df_pairs = df_pairs.sort_values(by="Count", ascending=False)
+
+    st.table(df_pairs)
+else:
+    st.info("Add at least 2 upgrade events to analyze patterns.")
+
